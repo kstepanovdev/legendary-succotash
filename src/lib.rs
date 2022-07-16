5daced55c9d355c998aa3de17c4b0a08fef51ca5
Kyrylo Stepanov <k.stepanov@tutanota.com>
2022-07-16 16:00:07 +0300

    tutorial 1 completed

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,72 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{near_bindgen, env};
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    // SETUP CONTRACT STATE
+    crossword_solution: String
 }
 
 #[near_bindgen]
 impl Contract {
-    // ADD CONTRACT METHODS HERE
+    #[init]
+    pub fn new(solution: String) -> Self {
+        Self {
+            crossword_solution: solution,
+        }
+    }
+
+    pub fn get_solution(&self) -> String {
+        self.crossword_solution.clone()
+    }
+    
+    pub fn guess_solution(&mut self, solution: String) -> bool {
+        let hashed_input = env::sha256(solution.as_bytes());
+        let hashed_input_hex = hex::encode(&hashed_input);
+
+        if hashed_input_hex == self.crossword_solution {
+            env::log_str("You guessed right!");
+            true
+        } else {
+            env::log_str("Try again.");
+            false
+        }
+    }
 }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
     use near_sdk::test_utils::{get_logs, VMContextBuilder};
     use near_sdk::{testing_env, AccountId};
 
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
     fn get_context(predecessor: AccountId) -> VMContextBuilder {
         let mut builder = VMContextBuilder::new();
         builder.predecessor_account_id(predecessor);
         builder
     }
+    
+    #[test]
+    fn check_guess_solution() {
+        // Get Alice as an account ID
+        let alice = AccountId::new_unchecked("alice.testnet".to_string());
+        // Set up the testing context and unit test environment
+        let context = get_context(alice);
+        testing_env!(context.build());
 
-    // TESTS HERE
+        // Set up contract object and call the new method
+        let mut contract = Contract::new(
+            "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
+        );
+        let mut guess_result = contract.guess_solution("wrong answer here".to_string());
+        assert!(!guess_result, "Expected a failure from the wrong guess");
+        assert_eq!(get_logs(), ["Try again."], "Expected a failure log.");
+        guess_result = contract.guess_solution("near nomicon ref finance".to_string());
+        assert!(guess_result, "Expected the correct answer to return true.");
+        assert_eq!(
+            get_logs(),
+            ["Try again.", "You guessed right!"],
+            "Expected a successful log after the previous failed log."
+        );
+    }
 }
