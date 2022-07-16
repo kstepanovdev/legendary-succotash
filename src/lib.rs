42721811eee37df09803bd9a9d1e3113adf7f01c
Kyrylo Stepanov <k.stepanov@tutanota.com>
2022-07-16 18:10:08 +0300

    second stable version

--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,276 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, env};
+use near_sdk::collections::{LookupMap, UnorderedSet};
+use near_sdk::{
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    log,
+    serde::{Deserialize, Serialize},
+    AccountId, PanicOnDefault, Promise,
+};
+use near_sdk::{env, near_bindgen};
 
-#[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    crossword_solution: String
+// 5 Ⓝ in yoctoNEAR
+const PRIZE_AMOUNT: u128 = 5_000_000_000_000_000_000_000_000;
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Puzzle {
+    status: PuzzleStatus,
+    answer: Vec<Answer>,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Answer {
+    num: u8,
+    start: CoordinatePair,
+    direction: AnswerDirection,
+    length: u8,
+    clue: String,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct CoordinatePair {
+    x: u8,
+    y: u8,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct JsonPuzzle {
+    solution_hash: String,
+    puzzle_status: PuzzleStatus,
+    answer: Vec<Answer>,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub enum PuzzleStatus {
+    Solved { memo: String },
+    Unsolved,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub enum AnswerDirection {
+    Across,
+    Down,
 }
 
 #[near_bindgen]
-impl Contract {
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Crossword {
+    owner_id: AccountId,
+    puzzles: LookupMap<String, Puzzle>,
+    unsolved_puzzles: UnorderedSet<String>,
+}
+
+#[near_bindgen]
+impl Crossword {
     #[init]
-    pub fn new(solution: String) -> Self {
+    pub fn new(owner_id: AccountId) -> Self {
         Self {
-            crossword_solution: solution,
+            owner_id,
+            puzzles: LookupMap::new(b"c"),
+            unsolved_puzzles: UnorderedSet::new(b"u"),
         }
     }
 
-    pub fn get_solution(&self) -> String {
-        self.crossword_solution.clone()
+    /// Get the hash of a crossword puzzle solution from the unsolved_puzzles
+    pub fn get_solution(&self, puzzle_index: u32) -> Option<String> {
+        let mut index = 0;
+        for puzzle_hash in self.unsolved_puzzles.iter() {
+            if puzzle_index == index {
+                return Some(puzzle_hash);
+            }
+            index += 1;
+        }
+        // Did not find that index
+        None
     }
-    
-    pub fn guess_solution(&mut self, solution: String) -> bool {
+
+    pub fn guess_solution(&mut self, solution: String, memo: String) {
         let hashed_input = env::sha256(solution.as_bytes());
         let hashed_input_hex = hex::encode(&hashed_input);
 
-        if hashed_input_hex == self.crossword_solution {
-            env::log_str("You guessed right!");
-            true
-        } else {
-            env::log_str("Try again.");
-            false
-        }
+        let mut puzzle = self
+            .puzzles
+            .get(&hashed_input_hex)
+            .expect("ERR_NOT_CORRECT_ANSWER");
+
+        puzzle.status = match puzzle.status {
+            PuzzleStatus::Unsolved => PuzzleStatus::Solved { memo: memo.clone() },
+            _ => env::panic_str("ERR_PUZZLE_SOLVED"),
+        };
+
+        self.puzzles.insert(&hashed_input_hex, &puzzle);
+        self.unsolved_puzzles.remove(&hashed_input_hex);
+
+        log!(
+            "Puzzle with solution hash {} solved, with memo: {}",
+            hashed_input_hex,
+            memo
+        );
+
+        // Transfer the prize money to the winner
+        Promise::new(env::predecessor_account_id()).transfer(PRIZE_AMOUNT);
+    }
+
+    pub fn new_puzzle(&mut self, solution_hash: String, answers: Vec<Answer>) {
+        assert_eq!(
+            env::predecessor_account_id(),
+            self.owner_id,
+            "Only the owner may call this method"
+        );
+
+        let existing = self.puzzles.insert(
+            &solution_hash,
+            &Puzzle {
+                status: PuzzleStatus::Unsolved,
+                answer: answers,
+            },
+        );
+        assert!(existing.is_none(), "Puzzle with that key already exists");
+        self.unsolved_puzzles.insert(&solution_hash);
+    }
+
+    pub fn submit_solution(&mut self, solution_hash: String, memo: String) {
+        let hashed_input = env::sha256(solution_hash.as_bytes());
+        let hashed_input_hex = hex::encode(&hashed_input);
+
+        // Check to see if the hashed answer is among the puzzles
+        let mut puzzle = self
+            .puzzles
+            .get(&hashed_input_hex)
+            .expect("ERR_NOT_CORRECT_ANSWER");
+
+        // Check if the puzzle is already solved. If it's unsolved, set the status to solved,
+        //   then proceed to update the puzzle and pay the winner.
+        puzzle.status = match puzzle.status {
+            PuzzleStatus::Unsolved => PuzzleStatus::Solved { memo: memo.clone() },
+            _ => {
+                env::panic_str("ERR_PUZZLE_SOLVED");
+            }
+        };
+
+        // Reinsert the puzzle back in after we modified the status:
+        self.puzzles.insert(&hashed_input_hex, &puzzle);
+        // Remove from the list of unsolved ones
+        self.unsolved_puzzles.remove(&hashed_input_hex);
+
+        log!(
+            "Puzzle with solution hash {} solved, with memo: {}",
+            hashed_input_hex,
+            memo
+        );
+
+        // Transfer the prize money to the winner
+        Promise::new(env::predecessor_account_id()).transfer(PRIZE_AMOUNT);
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
+    use near_sdk::test_utils::VMContextBuilder;
     use near_sdk::{testing_env, AccountId};
 
+    // part of writing unit tests is setting up a mock context
+    // provide a `predecessor` here, it'll modify the default context
     fn get_context(predecessor: AccountId) -> VMContextBuilder {
         let mut builder = VMContextBuilder::new();
         builder.predecessor_account_id(predecessor);
         builder
     }
-    
+
+    fn get_answers() -> Vec<Answer> {
+        vec![
+            Answer {
+                num: 1,
+                start: CoordinatePair { x: 2, y: 1 },
+                direction: AnswerDirection::Across,
+                length: 4,
+                clue: "Native token".to_string(),
+            },
+            Answer {
+                num: 1,
+                start: CoordinatePair { x: 2, y: 1 },
+                direction: AnswerDirection::Down,
+                length: 7,
+                clue: "Name of the specs/standards site is ______.io".to_string(),
+            },
+            Answer {
+                num: 2,
+                start: CoordinatePair { x: 5, y: 1 },
+                direction: AnswerDirection::Down,
+                length: 3,
+                clue: "DeFi site on NEAR is ___.finance".to_string(),
+            },
+            Answer {
+                num: 4,
+                start: CoordinatePair { x: 0, y: 7 },
+                direction: AnswerDirection::Across,
+                length: 7,
+                clue: "DeFi decentralizes this".to_string(),
+            },
+        ]
+    }
+
     #[test]
-    fn check_guess_solution() {
+    fn debug_get_hash() {
+        // Basic set up for a unit test
+        testing_env!(VMContextBuilder::new().build());
+
+        // Using a unit test to rapidly debug and iterate
+        let debug_solution = "near nomicon ref finance";
+        let debug_hash_bytes = env::sha256(debug_solution.as_bytes());
+        let debug_hash_string = hex::encode(debug_hash_bytes);
+        println!("Let's debug: {:?}", debug_hash_string);
+    }
+
+    #[test]
+    #[should_panic(expected = "ERR_NOT_CORRECT_ANSWER")]
+    fn check_submit_solution_failure() {
         // Get Alice as an account ID
         let alice = AccountId::new_unchecked("alice.testnet".to_string());
         // Set up the testing context and unit test environment
-        let context = get_context(alice);
+        let context = get_context(alice.clone());
         testing_env!(context.build());
 
         // Set up contract object and call the new method
-        let mut contract = Contract::new(
+        let mut contract = Crossword::new(alice);
+        // Add puzzle
+        let answers = get_answers();
+        contract.new_puzzle(
             "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
+            answers,
         );
-        let mut guess_result = contract.guess_solution("wrong answer here".to_string());
-        assert!(!guess_result, "Expected a failure from the wrong guess");
-        assert_eq!(get_logs(), ["Try again."], "Expected a failure log.");
-        guess_result = contract.guess_solution("near nomicon ref finance".to_string());
-        assert!(guess_result, "Expected the correct answer to return true.");
-        assert_eq!(
-            get_logs(),
-            ["Try again.", "You guessed right!"],
-            "Expected a successful log after the previous failed log."
+        contract.submit_solution("wrong answer here".to_string(), "my memo".to_string());
+    }
+
+    #[test]
+    fn check_submit_solution_success() {
+        // Get Alice as an account ID
+        let alice = AccountId::new_unchecked("alice.testnet".to_string());
+        // Set up the testing context and unit test environment
+        let context = get_context(alice.clone());
+        testing_env!(context.build());
+
+        // Set up contract object
+        let mut contract = Crossword::new(alice);
+
+        // Add puzzle
+        let answers = get_answers();
+        contract.new_puzzle(
+            "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
+            answers,
         );
+
+        contract.submit_solution(
+            "near nomicon ref finance".to_string(),
+            "my memo".to_string(),
+        );
+
+        // Ensure the puzzle status is now Solved
+        // contract.get_puzzle_status("69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string());
+        // assert_eq!(contract.unsolved_puzzles.len(), 0, "Should not have any unsolved puzzles.");
     }
 }
